1168bb3aedc4c7365599e90d692177991970fba9
WontonLov3r <danielhariantowu@gmail.com>
2023-03-22 11:09:29 -0400

    yes

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ states.onchange = function () {
     //Concatenates the string of the image producing the string : img/@2x.jpg with the actual value in the middle
     document.getElementById('postcard').style.backgroundImage = 'url(img/' + states.value + '@2x.jpg)'
 
-    //select the innerHTML of element with hte ID "state-heading", and assign it with the value of the selected state from the drop down menu 
+    //select the innerHTML of element with the ID "state-heading", and assign it with the value of the selected state from the drop down menu 
     document.getElementById('state-heading').innerHTML = states.options[states.selectedIndex].text
 
-
+    //select the innerHTML of element with the ID "greeting", and assign it with the value of "Greetings from:"
     document.getElementById('greeting').innerHTML = 'Greetings from: '
 
 
